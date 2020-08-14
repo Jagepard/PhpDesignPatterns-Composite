188891c3899fae4d22c9cfee8ef3dcc16a685f1c
jagepard <dankorot@gmail.com>
2020-08-14 06:46:25 -0700

    Update

--- tests/CompositeTest.php
+++ tests/CompositeTest.php
@@ -3,50 +3,37 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Structural\Composite\Tests;
 
-use Structural\Composite\AbstractComponent;
-use Structural\Composite\Leaf;
-use Structural\Composite\Composite;
+use Structural\Composite\{AbstractComponent, Leaf, Composite};
 use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
 
 class CompositeTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Composite
-     */
-    private $root;
+    private Composite $root;
 
     protected function setUp(): void
     {
-        $this->root = new Composite('root');
+        $this->root = new Composite("root");
     }
 
     public function testComposite()
     {
         $this->assertInstanceOf(AbstractComponent::class, $this->root);
-        $this->getRoot()->add(new Composite('root'));
-        $this->assertInstanceOf(Composite::class, $this->getRoot()->getChild('root'));
-        $this->assertEquals('root', $this->getRoot()->getChild('root')->getName());
-        $this->getRoot()->remove('root');
+        $this->root->add(new Composite("root"));
+        $this->assertInstanceOf(Composite::class, $this->root->getChild("root"));
+        $this->assertEquals("root", $this->root->getChild("root")->getName());
+        $this->root->remove("root");
     }
 
     public function testLeaf()
     {
-        $this->getRoot()->add(new Composite('root'));
-        $this->getRoot()->getChild('root')->add(new Leaf('first'));
-        $this->assertInstanceOf(Leaf::class, $this->getRoot()->getChild('root')->getChild('first'));
-    }
-
-    /**
-     * @return Composite
-     */
-    public function getRoot(): Composite
-    {
-        return $this->root;
+        $this->root->add(new Composite("root"));
+        $this->root->getChild("root")->add(new Leaf("first"));
+        $this->assertInstanceOf(Leaf::class, $this->root->getChild("root")->getChild("first"));
     }
 }
